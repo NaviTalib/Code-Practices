eb9dc30b97a2c7f6be85c6351e5f734e4b9b86e2
Navi Talib <talibali786.ta@gmail.com>
2023-03-30 22:13:00 +0530

    calcjs file is updated

--- coding-in-nodejs/calculator-njs/calc.js
+++ coding-in-nodejs/calculator-njs/calc.js
@@ -1,10 +1,21 @@
 const express = require('express');
+//Using body-parser allows you to access request
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/",function(req,res){
+    var n1 = Number(req.body.num1);
+    var n2 = Number(req.body.num2);
+
+    var result = n1 + n2;
+    res.send("The addition of two numbers is : "+result);
+})
 
 app.listen(3000,function(){
         console.log("server is running on port 3000..")
