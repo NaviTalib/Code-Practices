0dac25a0392bef51e0077ca9ccddfac304ec578f
Navi Talib <talibali786.ta@gmail.com>
2024-02-25 20:17:36 +0530

    string added

---
+++ javacodes/Day 6/NewStr.java
@@ -0,0 +1,9 @@
+public class NewStr {
+    public static void main(String[] args) {
+        String newstr = "Hello";
+        String newstr2 = " world";
+
+        System.out.println(newstr+newstr2);
+        System.out.println(newstr.length());
+    }
+}
